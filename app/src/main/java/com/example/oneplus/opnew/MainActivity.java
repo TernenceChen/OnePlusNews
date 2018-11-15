8b750c413369ce26cf1c232bf2bee37e749ff03f
Chen <xdchenteng@163.com>
2018-11-15 16:12:45 +0800

    更新数据库的更新操作

--- app/src/main/java/com/example/oneplus/opnew/MainActivity.java
+++ app/src/main/java/com/example/oneplus/opnew/MainActivity.java
@@ -23,7 +23,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        newsHelper = new NewsHelper(this,"History",null,1);
+        updateTable();
         for (int i = 0; i < 3; i++){
             View view = LayoutInflater.from(this).inflate(R.layout.tab_widget_indicator,null,false);
             TextView textView = view.findViewById(R.id.indicator);
@@ -84,6 +85,23 @@ public class MainActivity extends AppCompatActivity {
         int year = calendar.get(Calendar.YEAR);
         int month = calendar.get(Calendar.MONTH) + 1;
         int day = calendar.get(Calendar.DAY_OF_MONTH) - 3;
+        if (day <= 0){
+            if (month == 3){
+                if (year%4 == 0) {
+                    day = day + 29;
+                }else{
+                    day = day + 28;
+                }
+            }else if (month == 5 || month == 7 || month == 10 || month == 12){
+                day = day + 30;
+            }else if (month == 1){
+                day = day + 31;
+                year = year - 1;
+            }else{
+                day = day + 31;
+            }
+            month = month - 1;
+        }
         time = day + month * 100 + year * 10000;
         return time;
     }
